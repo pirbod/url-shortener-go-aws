1733aba7a5cb36cfc34ae9dbcf1e3e02906655ee
Parham <36266372+pirbod@users.noreply.github.com>
2025-06-26 22:02:59 +0200

    fix/correct WAF ACL visibility_config block

--- infra/modules/security/waf.tf
+++ infra/modules/security/waf.tf
@@ -1,8 +1,12 @@
 resource "aws_wafv2_web_acl" "main" {
   name  = "${var.env}-waf"
   scope = "REGIONAL"
-  default_action { allow {} }
 
+  default_action {
+    allow {}
+  }
+
+  # Global visibility configuration for the entire ACL
   visibility_config {
     cloudwatch_metrics_enabled = true
     sampled_requests_enabled   = true
@@ -12,13 +16,19 @@ resource "aws_wafv2_web_acl" "main" {
   rule {
     name     = "AWSManagedRulesCommonRuleSet"
     priority = 1
-    override_action { none {} }
+
+    override_action {
+      none {}
+    }
+
     statement {
       managed_rule_group_statement {
         name        = "AWSManagedRulesCommonRuleSet"
         vendor_name = "AWS"
       }
     }
+
+    # Required per-rule visibility config
     visibility_config {
       cloudwatch_metrics_enabled = true
       sampled_requests_enabled   = true
@@ -27,6 +37,7 @@ resource "aws_wafv2_web_acl" "main" {
   }
 }
 
+# Associate the WAF Web ACL with the ALB
 resource "aws_wafv2_web_acl_association" "link" {
   resource_arn = var.alb_arn
   web_acl_arn  = aws_wafv2_web_acl.main.arn
